049c3481eee3e7b68fa72ff5539b162719d3ac3a
Agustin DAngelo <agustindangelo2113@gmail.com>
2020-08-09 16:26:36 -0300

    página paciente-hoy y fixes varios

--- TP Java/web/html/scripts.js
+++ TP Java/web/html/scripts.js
@@ -1,4 +1,3 @@
-
 function filtrarPacientes() {
   // Declare variables
   var input, filter, ul, li, a, i;
@@ -17,6 +16,41 @@ function filtrarPacientes() {
     }
   }
 }
+
+  function filtrarAlimentos() {
+	// Declare variables
+	var input, filter, ul, li, i;
+	input = document.getElementById("alimentosSearch");
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("alimentosMenu");
+	li = ul.getElementsByTagName("li");
+  
+	// Loop through all list items, and hide those who don't match the search query
+	for (i = 0; i < li.length; i++) {
+	  if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+		li[i].style.display = "";
+	  } else {
+		li[i].style.display = "none";
+	  }
+	}
+  }
+  function filtrarEjercicios() {
+	// Declare variables
+	var input, filter, ul, li, i;
+	input = document.getElementById("ejerciciosSearch");
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("ejerciciosMenu");
+	li = ul.getElementsByTagName("li");
+  
+	// Loop through all list items, and hide those who don't match the search query
+	for (i = 0; i < li.length; i++) {
+	  if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+		li[i].style.display = "";
+	  } else {
+		li[i].style.display = "none";
+	  }
+	}
+  }
 
 // Listado editable de horarios
 $(document).ready(function(){
