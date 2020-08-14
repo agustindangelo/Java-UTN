c1de9a27c1905283b4d0d94701b174bc279ea4d4
Agustin DAngelo <agustindangelo2113@gmail.com>
2020-08-14 11:35:25 -0300

    responsiveando nutricionista-main

--- TP Java/web/html/scripts.js
+++ TP Java/web/html/scripts.js
@@ -1,3 +1,18 @@
+$(function(){
+	$("#menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	});
+
+	$(window).resize(function(e) {
+		if($(window).width()<=768){
+			$("#wrapper").removeClass("toggled");
+		}else{
+			$("#wrapper").addClass("toggled");
+		}
+	});
+});
+   
 function filtrarPacientes() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("pacienteSearch");
